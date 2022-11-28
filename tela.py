826383352fa1bb6f2519c7b63964a59b3fa9ac9c
Lupesms97 <lupesms97@gmail.com>
2022-11-28 09:44:17 -0300

    adicionando parte gra

---
+++ tela.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from Pessoas import *
+
+
+janela = Tk()
+janela.title('CADASTRO DO CLIENTE')
+
+textoOrientacao = Label(janela,text='Oque você desejar fazer ?')
+textoOrientacao.grid(column=0, row=0)
+botaoInsserir = Button(janela, text= '[1] Inserir novo cliente', command=))
+
+textoOrientacao1 = Label(janela, text='[2] Exibir Lista')
+textoOrientacao1.grid(column=0, row=1)
+
+
+
+
+
+janela.mainloop()
